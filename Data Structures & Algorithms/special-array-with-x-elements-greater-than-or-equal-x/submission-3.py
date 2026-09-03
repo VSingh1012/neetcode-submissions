52e35a64121473237f0ff2987563779210494132
VSingh1012 <72770416+VSingh1012@users.noreply.github.com>
2026-09-02 22:13:54 -0400

    Add: special-array-with-x-elements-greater-than-or-equal-x - submission-3

---
+++ Data Structures & Algorithms/special-array-with-x-elements-greater-than-or-equal-x/submission-3.py
@@ -0,0 +1,50 @@
+class Solution:
+    def specialArray(self, nums: List[int]) -> int:
+        # i = 1 # set our i to 1
+        n = len(nums) # for readability=
+        max_num = max(nums) + 1 # O(n)
+
+        # max_num: 5
+        # i: 2
+        # n: 2
+        # j: 0
+        # count: 2
+
+        # while i < max_num:
+        #     count = 0
+        #     j = 0
+        #     while j < n: # O(n)
+        #         if nums[j] >= i:
+        #             count += 1
+        #         j += 1
+            
+        #     if count == i:
+        #         return count
+        #     i += 1
+
+        for i in range(1,max_num):
+            count = 0
+            for j in range(n):
+                if nums[j] >= i:
+                    count += 1
+            
+            if count == i:
+                return count
+
+
+            
+
+        # time complexity: O(n * max_num)
+                 
+                
+        return -1
+    
+
+
+    
+
+            
+
+            
+            
+
